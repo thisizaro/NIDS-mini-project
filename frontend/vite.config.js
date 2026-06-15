73bb95a16ebde9030baab3c0044a95ba9ec42e4c
thisizaro <thisisaro.official@gmail.com>
2026-06-15 21:35:32 +0530

    docker-compose push.

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -9,7 +9,7 @@ export default defineConfig({
     port: 5173,
     proxy: {
       "/api": {
-        target: "http://localhost:8080",
+        target: "http://orchestrator:8080",
         changeOrigin: true,
       },
     },
--- model_service/app/config.py
+++ model_service/app/config.py
@@ -1,12 +1,24 @@
 import os
 
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-PROJECT_ROOT = os.path.dirname(BASE_DIR)
+# BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+# PROJECT_ROOT = os.path.dirname(BASE_DIR)
 
-MODEL_PATH = os.path.join(PROJECT_ROOT, "trained_models", "cnn_openmax", "best_model.keras")
-OPENMAX_PARAMS_PATH = os.path.join(PROJECT_ROOT, "trained_models", "cnn_openmax", "openmax_params.json")
-SCALER_PATH = os.path.join(PROJECT_ROOT, "trained_models", "preprocessing", "scaler_infogan_kaggle.pkl")
-FEATURE_COLS_PATH = os.path.join(PROJECT_ROOT, "trained_models", "preprocessing", "infogan_kaggle_feature_cols.json")
+# MODEL_PATH = os.path.join(PROJECT_ROOT, "trained_models", "cnn_openmax", "best_model.keras")
+# OPENMAX_PARAMS_PATH = os.path.join(PROJECT_ROOT, "trained_models", "cnn_openmax", "openmax_params.json")
+# SCALER_PATH = os.path.join(PROJECT_ROOT, "trained_models", "preprocessing", "scaler_infogan_kaggle.pkl")
+# FEATURE_COLS_PATH = os.path.join(PROJECT_ROOT, "trained_models", "preprocessing", "infogan_kaggle_feature_cols.json")
+
+
+
+# Explicit base path for models (Docker-friendly)
+MODEL_BASE_PATH = os.getenv("MODEL_BASE_PATH", "/app/trained_models")
+
+MODEL_PATH = os.path.join(MODEL_BASE_PATH, "cnn_openmax", "best_model.keras")
+OPENMAX_PARAMS_PATH = os.path.join(MODEL_BASE_PATH, "cnn_openmax", "openmax_params.json")
+SCALER_PATH = os.path.join(MODEL_BASE_PATH, "preprocessing", "scaler_infogan_kaggle.pkl")
+FEATURE_COLS_PATH = os.path.join(MODEL_BASE_PATH, "preprocessing", "infogan_kaggle_feature_cols.json")
+
+
 
 NUM_KNOWN_CLASSES = 6
 ALPHA_RANK = 5
